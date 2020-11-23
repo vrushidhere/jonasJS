9db1a8113bb8dcae9d250ea318debe7f8bad460e
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-23 19:17:26 +0530

    let var const fundamental

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,7 @@
-var country = 'India';
-var continent = 'Asia';
-var population = '12500M';
+let language = 'Marathi';
+const country = 'India';
 
-var isIsland = true;
-var language;
+language = 'Hindi';
+//country = 'Nepal';
 
-console.log('country: ' + country + ', continent: ' + continent + ', population: ' + population);
-
-console.log( typeof isIsland + ', ' +  typeof population + ', ' +  typeof country + ', ' +  typeof language );
+console.log(language + ', ' + country);
