652ca1c9a1a0eb5be98f4066e5c55c855f3245b5
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-23 17:17:20 +0530

    data type assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,4 +2,9 @@ var country = 'India';
 var continent = 'Asia';
 var population = '12500M';
 
+var isIsland = true;
+var language;
+
 console.log('country: ' + country + ', continent: ' + continent + ', population: ' + population);
+
+console.log( typeof isIsland + ', ' +  typeof population + ', ' +  typeof country + ', ' +  typeof language );
