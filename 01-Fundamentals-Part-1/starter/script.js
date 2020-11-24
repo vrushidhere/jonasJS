04d0dad610b762376180f0f47a3bfd8b9836a9ce
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-24 17:34:07 +0530

    template literals example

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,18 +1,7 @@
-/*const marksWeight = 78;
-const marksHeight = 1.69;
+const description = `
+Portugal is in Europe, 
+and its 11 million
+people speak 
+portuguese`;
 
-const johnsWeight = 92;
-const johnsHeight = 1.95;*/
-
-const marksWeight = 95;
-const marksHeight = 1.88;
-
-const johnsWeight = 85;
-const johnsHeight = 1.76;
-
-let marksBMI = marksWeight / (marksHeight ** 2);
-let johnsBMI = johnsWeight / (johnsHeight ** 2);
-
-let markHigherBMI = (marksBMI > johnsBMI);
-
-console.log(markHigherBMI, marksBMI, johnsBMI);
+console.log('template literals exaple: ' + description);
