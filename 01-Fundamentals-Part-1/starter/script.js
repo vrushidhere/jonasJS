a364a7a434ef84c0a8d6be125566aec88681cf87
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-24 16:26:06 +0530

    BMI calculation

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,24 +1,18 @@
-let x = 1+ 10;
-x += 10; // this is x = x + 10
+/*const marksWeight = 78;
+const marksHeight = 1.69;
 
-x++; // this is x = x + 1
+const johnsWeight = 92;
+const johnsHeight = 1.95;*/
 
-// now x is 22
-x = x++ + 2; // this returns x as 22
-x = ++x + 2; // this returns x as 23 so it ads 1 value to x before opertion 
+const marksWeight = 95;
+const marksHeight = 1.88;
 
+const johnsWeight = 85;
+const johnsHeight = 1.76;
 
-console.log(x);
+let marksBMI = marksWeight / (marksHeight ** 2);
+let johnsBMI = johnsWeight / (johnsHeight ** 2);
 
+let markHigherBMI = (marksBMI > johnsBMI);
 
-let country = 100;
-let halfpopulation = (country / 2);
-let finland = 600;
-let avergePopulation = 33;
-const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
-
-console.log(halfpopulation);
-console.log(++country);
-console.log(country > finland);
-console.log(country < avergePopulation);
-console.log(description);
+console.log(markHigherBMI, marksBMI, johnsBMI);
