81405f730fe0ff7e6a30711c951bcaa43cc1fd05
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-24 16:18:14 +0530

    basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,24 @@
-let language = 'Marathi';
-const country = 'India';
+let x = 1+ 10;
+x += 10; // this is x = x + 10
 
-language = 'Hindi';
-//country = 'Nepal';
+x++; // this is x = x + 1
 
-console.log(language + ', ' + country);
+// now x is 22
+x = x++ + 2; // this returns x as 22
+x = ++x + 2; // this returns x as 23 so it ads 1 value to x before opertion 
+
+
+console.log(x);
+
+
+let country = 100;
+let halfpopulation = (country / 2);
+let finland = 600;
+let avergePopulation = 33;
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+
+console.log(halfpopulation);
+console.log(++country);
+console.log(country > finland);
+console.log(country < avergePopulation);
+console.log(description);
