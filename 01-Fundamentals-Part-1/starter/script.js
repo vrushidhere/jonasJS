1ff55c3d666675530a1075637f505386619e7fae
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-25 19:57:47 +0530

    conversionCoercion practice

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,18 +1,5 @@
-/*const marksWeight = 78;
-const marksHeight = 1.69;
-
-const johnsWeight = 92;
-const johnsHeight = 1.95;*/
-
-const marksWeight = 95;
-const marksHeight = 1.88;
-
-const johnsWeight = 85;
-const johnsHeight = 1.76;
-
-let marksBMI = marksWeight / (marksHeight ** 2);
-let johnsBMI = johnsWeight / (johnsHeight ** 2);
-
-let markHigherBMI = (marksBMI > johnsBMI);
-
-console.log(markHigherBMI, marksBMI, johnsBMI);
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // true
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 117 actual 1143
