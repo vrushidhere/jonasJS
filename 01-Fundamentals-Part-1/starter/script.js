cf3a0b98df63a5d413f9e16f142a74aa7e4cd3c6
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-25 20:55:44 +0530

    switchStatement assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,22 @@
-let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+const language = 'hindi';
 
-if (numNeighbours === 1){
-    console.log('Only 1 border!');
-} else if (numNeighbours > 1) {
-    console.log('More than 1 border');
-} else {
-    console.log('No border');
+switch(language) {
+    case 'chineese':
+        console.log('Most number of native spearker!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+
+    default:
+        console.log('Great language too :D');
 }
