135a8f073a146111dce70c0d20e25b9004409f47
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-26 09:37:05 +0530

    coding challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,34 @@
-const countryPopulation = 14;
+// const dolphineScore1 = 96;
+// const dolphineScore2 = 108;
+// const dolphineScore3 = 89;
 
-var averagePopulation = countryPopulation > 33 ? 'Portugal\'s population is above average.' : 'Portugal\'s population is below average';
+// const koalasScrore1 = 88;
+// const koalasScrore2 = 91;
+// const koalasScrore3 = 110;
 
-console.log(averagePopulation);
+// const dolphineScore1 = 97;
+// const dolphineScore2 = 112;
+// const dolphineScore3 = 101;
 
-console.log(`Portugal's population is ${countryPopulation > 33 ? 'above' : 'below'} average`)
+// const koalasScrore1 = 109;
+// const koalasScrore2 = 95;
+// const koalasScrore3 = 103;
+
+const dolphineScore1 = 101;
+const dolphineScore2 = 101;
+const dolphineScore3 = 101;
+
+const koalasScrore1 = 101;
+const koalasScrore2 = 101;
+const koalasScrore3 = 101;
+
+const dolphineAverageScrore = (dolphineScore1 + dolphineScore2 + dolphineScore3) / 3;
+const koalasAverageScore = (koalasScrore1 + koalasScrore2 + koalasScrore3) / 3;
+
+if (dolphineAverageScrore > koalasAverageScore && dolphineAverageScrore > 100) {
+    console.log('dolhpne is winner!');
+} else if (dolphineAverageScrore < koalasAverageScore && koalasAverageScore > 100) {
+    console.log('Koalas is winner!');
+} else if (dolphineAverageScrore === koalasAverageScore && dolphineAverageScrore > 100 && koalasAverageScore > 100) {
+    console.log('Match is draw!');
+}
