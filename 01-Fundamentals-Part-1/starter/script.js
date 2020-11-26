7db8ad23d28b95cd48f923ce01b6741797c41e41
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-26 08:58:47 +0530

    another option

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,7 @@
-const countryPopulation = 34;
+const countryPopulation = 14;
 
 var averagePopulation = countryPopulation > 33 ? 'Portugal\'s population is above average.' : 'Portugal\'s population is below average';
 
 console.log(averagePopulation);
+
+console.log(`Portugal's population is ${countryPopulation > 33 ? 'above' : 'below'} average`)
