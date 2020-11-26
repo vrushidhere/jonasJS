f9bce7dbaae7c9d3ecf2a0b2ca8d325ffe2f5604
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-11-26 15:03:45 +0530

    coding challenge 4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,34 +1,4 @@
-// const dolphineScore1 = 96;
-// const dolphineScore2 = 108;
-// const dolphineScore3 = 89;
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-// const koalasScrore1 = 88;
-// const koalasScrore2 = 91;
-// const koalasScrore3 = 110;
-
-// const dolphineScore1 = 97;
-// const dolphineScore2 = 112;
-// const dolphineScore3 = 101;
-
-// const koalasScrore1 = 109;
-// const koalasScrore2 = 95;
-// const koalasScrore3 = 103;
-
-const dolphineScore1 = 101;
-const dolphineScore2 = 101;
-const dolphineScore3 = 101;
-
-const koalasScrore1 = 101;
-const koalasScrore2 = 101;
-const koalasScrore3 = 101;
-
-const dolphineAverageScrore = (dolphineScore1 + dolphineScore2 + dolphineScore3) / 3;
-const koalasAverageScore = (koalasScrore1 + koalasScrore2 + koalasScrore3) / 3;
-
-if (dolphineAverageScrore > koalasAverageScore && dolphineAverageScrore > 100) {
-    console.log('dolhpne is winner!');
-} else if (dolphineAverageScrore < koalasAverageScore && koalasAverageScore > 100) {
-    console.log('Koalas is winner!');
-} else if (dolphineAverageScrore === koalasAverageScore && dolphineAverageScrore > 100 && koalasAverageScore > 100) {
-    console.log('Match is draw!');
-}
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
