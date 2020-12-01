9787b00a87f5ec734693f4a21a9bbc8b0e01a7a3
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-01 23:09:53 +0530

    arrayOperations assignments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,13 @@
-const populations = [10,12,15,24];
-console.log(`${populations.length === 4}`);
+const neighbours = ['India', 'Pakistan', 'China'];
 
-const totalPopulation = populations.reduce((a, b) => a + b, 0);
+neighbours.push('Utopia');
 
-// console.log(populations[0]/totalPopulation * 100 + '%')
-// console.log(populations[1]/totalPopulation * 100 + '%')
-// console.log(populations[2]/totalPopulation * 100 + '%')
-// console.log(populations[3]/totalPopulation * 100 + '%')
+neighbours.pop();
 
-for(i = 0 ; i < populations.length; i++ ) {
-    console.log(i);
-    console.log(populations[i]/totalPopulation * 100 + '%');
-}
+console.log(`${!neighbours.includes('Germany') ? 'Probably not a central European country :D' : ''}`);
+
+const chinaIndex = neighbours.indexOf('China');
+
+neighbours[chinaIndex] = 'Bhutan';
+
+console.log(neighbours);
