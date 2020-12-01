df08b307fb585630d12dfb3bee50fc1506ff226e
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-01 22:11:11 +0530

    creative assignment solution

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,7 +3,12 @@ console.log(`${populations.length === 4}`);
 
 const totalPopulation = populations.reduce((a, b) => a + b, 0);
 
-console.log(populations[0]/totalPopulation * 100 + '%')
-console.log(populations[1]/totalPopulation * 100 + '%')
-console.log(populations[2]/totalPopulation * 100 + '%')
-console.log(populations[3]/totalPopulation * 100 + '%')
+// console.log(populations[0]/totalPopulation * 100 + '%')
+// console.log(populations[1]/totalPopulation * 100 + '%')
+// console.log(populations[2]/totalPopulation * 100 + '%')
+// console.log(populations[3]/totalPopulation * 100 + '%')
+
+for(i = 0 ; i < populations.length; i++ ) {
+    console.log(i);
+    console.log(populations[i]/totalPopulation * 100 + '%');
+}
