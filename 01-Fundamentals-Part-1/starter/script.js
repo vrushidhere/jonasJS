4ce3870168442c5b033bcb2bcf49d7b57da00274
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-01 12:05:34 +0530

    arrowFunction assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,18 @@
 const worldPopulation = 7900;
 
-const percentageOfWorld1 = function(population) {
-    return ( population / 7900 ) * 100;
-}
+// const percentageOfWorld1 = function(population) {
+//     return ( population / 7900 ) * 100;
+// }
 
-const percentageOfWorld2 = function(population) {
-    return ( population / 7900 ) * 100;
-}
+// const percentageOfWorld2 = function(population) {
+//     return ( population / 7900 ) * 100;
+// }
 
-const ChinaPopulation = percentageOfWorld1(1900);
-const IndiaPopulation = percentageOfWorld1(1500);
-const NepalPopulation = percentageOfWorld1(12);
+const percentageOfWorld3 = population => (population / worldPopulation) * 100;
+
+const ChinaPopulation = percentageOfWorld3(1900);
+const IndiaPopulation = percentageOfWorld3(1500);
+const NepalPopulation = percentageOfWorld3(12);
 
 console.log(`
     China Population : ${ChinaPopulation}%,
