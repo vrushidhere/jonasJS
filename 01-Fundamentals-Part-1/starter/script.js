efebe9ad26f7abe2c411cefb29a5057ffa177099
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-01 23:45:22 +0530

    coding challenge 6

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,11 @@
-const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const bill = [125, 555, 44];
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const tip = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
+
+console.log(tip);
+
+//console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
