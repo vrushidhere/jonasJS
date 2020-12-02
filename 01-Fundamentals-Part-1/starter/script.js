10c8bdb187391edbd88670e7bb40584b4b5f44ad
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-02 16:22:12 +0530

    dot vs bracket notation

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,6 +2,15 @@ const myCountry = {
     country: 'India',
     capital: 'Delhi',
     language: 'Hindi',
-    population: '125',
+    population: 125,
     neighbours: ['Pakistan', 'Bhutan', 'China', 'Nepal', 'Bangladesh']
 }
+
+console.log(`
+${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries
+and a capital called ${myCountry.capital}.'
+`);
+
+console.log(myCountry.population += 2);
+
+console.log(myCountry['population'] -= 2);
