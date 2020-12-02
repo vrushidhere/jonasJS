4a3a54bb58904fc6350a490f1cd0e95918923272
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-02 22:50:02 +0530

    objectMethods assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,14 +3,15 @@ const myCountry = {
     capital: 'Delhi',
     language: 'Hindi',
     population: 125,
-    neighbours: ['Pakistan', 'Bhutan', 'China', 'Nepal', 'Bangladesh']
-}
+    neighbours: ['Pakistan', 'Bhutan', 'China', 'Nepal', 'Bangladesh'],
+    describe: function() {
+        this.countryName = this.country;
+        return `${this.country} has ${this.population} million ${this.language}-speaking people, 
+        ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
+    },
+    checkIsland: function() {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
+};
 
-console.log(`
-${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries
-and a capital called ${myCountry.capital}.'
-`);
-
-console.log(myCountry.population += 2);
-
-console.log(myCountry['population'] -= 2);
+console.log(myCountry.describe());
