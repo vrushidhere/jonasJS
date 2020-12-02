946348a376a6683700bc3b4c6511355031aca6d5
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-02 16:11:55 +0530

    object introduction assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,7 @@
-const bill = [125, 555, 44];
-
-const calcTip = function(bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const myCountry = {
+    country: 'India',
+    capital: 'Delhi',
+    language: 'Hindi',
+    population: '125',
+    neighbours: ['Pakistan', 'Bhutan', 'China', 'Nepal', 'Bangladesh']
 }
-
-const tip = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
-
-console.log(tip);
-
-//console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
