0d25b6458047a99707c413dd50d927096962c411
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-03 20:44:18 +0530

    coding challenge 7

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,32 @@
-for (i = 1; i <=50; i++){
-    console.log(`Voter number ${i} is currently voting`);
+// const marksWeight = 95;
+// const marksHeight = 1.88;
+
+// const johnsWeight = 85;
+// const johnsHeight = 1.76;
+
+const marks = {
+    height : 85,
+    weight : 1.88,
+    calcBMI: function() {
+        this.bmi = this.weight / (this.height ** 2);
+        return this.bmi;
+    }    
+}
+
+const johns = {
+    height : 85,
+    weight : 1.88,
+    calcBMI: function() {
+        this.bmi = this.weight / (this.height ** 2);
+        return this.bmi
+    }    
+}
+
+marks.calcBMI();
+johns.calcBMI();
+
+if(marks.bmi > johns.bmi){
+    console.log(`Mark's BMI ${this.marks.bmi} is higher than John's BMI ${johns.bmi}`);
+} else {
+    console.log(`Mark's BMI ${marks.bmi} is lesser than John's BMI ${johns.bmi}`)
 }
