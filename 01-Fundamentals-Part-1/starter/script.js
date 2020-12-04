a3bdf98edee3aec77917a0f91affba520e652a7f
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-04 10:52:05 +0530

    coding challenge 8

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,33 @@
-const bill = [125, 555, 44];
+const bill = [125, 555, 44, 33, 24, 78, 44, 900, 120, 342, 456, 34];
 
-const calcTip = function(bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const tips = [];
+
+const totals = [];
+
+const calcTip = function (billAmount) {
+  return billAmount >= 50 && billAmount <= 300
+    ? billAmount * 0.15
+    : billAmount * 0.2;
+};
+
+for (let i = 0; i < bill.length; i++) {
+  const tip = calcTip(bill[i]);
+  tips.push(tip);
+  totals.push(tip + bill[i]);
 }
 
-const tip = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
+console.log(tips);
+console.log(totals);
 
-console.log(tip);
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+  console.log(sum);
+};
+
+console.log(calcAverage(totals));
 
 //console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
