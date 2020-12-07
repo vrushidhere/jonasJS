598fec87a40f970a73620be7e79de28083134777
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-07 16:52:52 +0530

    click event practice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  document.querySelector('.message').textContent =
+    'You entered ' + document.querySelector('.guess').value;
+});
