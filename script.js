ece3cfcbe5ef228e7aac7f499f95053eacd5fde4
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-08 16:06:14 +0530

    guessGame implementation

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 'use strict';
 
-document.querySelector('.check').addEventListener('click', function () {
-  document.querySelector('.message').textContent =
-    'You entered ' + document.querySelector('.guess').value;
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+let message = document.querySelector('.message').textContent;
+let score = Number(document.querySelector('.score').textContent);
+let highscore = Number(document.querySelector('.highscore').textContent);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+document.querySelector('.number').textContent = secretNumber;
+
+checkBtn.addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+
+  function newMessage(message) {
+    document.querySelector('.message').textContent = message;
+  }
+
+  function guessUpdate(guess) {
+    if (guess > secretNumber) {
+      newMessage('Too High!');
+      document.querySelector('.score').textContent = --score;
+    } else if (guess < secretNumber) {
+      newMessage('Too Low!');
+      document.querySelector('.score').textContent = --score;
+    } else if (guess === secretNumber) {
+      newMessage('Correct Answer');
+      highscore < score ? (highscore = score) : highscore;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+
+  if (!guess) {
+    newMessage('You Must Guess...');
+  } else {
+    guessUpdate(guess);
+  }
+});
+
+againBtn.addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start Guessing..';
+  document.querySelector('.score').textContent = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
 });
