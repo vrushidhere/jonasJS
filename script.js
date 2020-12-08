48e276d7c36a127724e0ab62fc4f501665ed264b
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-08 19:40:52 +0530

    remaining functionality

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let score = Number(document.querySelector('.score').textContent);
 let highscore = Number(document.querySelector('.highscore').textContent);
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
-document.querySelector('.number').textContent = secretNumber;
+//document.querySelector('.number').textContent = secretNumber;
 
 checkBtn.addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -27,18 +27,29 @@ checkBtn.addEventListener('click', function () {
       newMessage('Correct Answer');
       highscore < score ? (highscore = score) : highscore;
       document.querySelector('.highscore').textContent = highscore;
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('body').style.backgroundColor = 'green';
+      document.querySelector('.number').style.width = '30rem';
     }
   }
 
   if (!guess) {
     newMessage('You Must Guess...');
-  } else {
+  } else if (score > 1) {
     guessUpdate(guess);
+  } else {
+    newMessage('You Loose');
+    document.querySelector('body').style.backgroundColor = 'red';
+    document.querySelector('.score').textContent = 0;
   }
 });
 
 againBtn.addEventListener('click', function () {
   document.querySelector('.message').textContent = 'Start Guessing..';
+  score = 20;
   document.querySelector('.score').textContent = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
