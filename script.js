ea2a3176f70caa7046874ef35692018d9bdc3eaf
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-14 20:08:28 +0530

    modal assignment

--- script.js
+++ script.js
@@ -1,55 +1,29 @@
 'use strict';
 
-const checkBtn = document.querySelector('.check');
-const againBtn = document.querySelector('.again');
-let message = document.querySelector('.message').textContent;
-let score = Number(document.querySelector('.score').textContent);
-let highscore = Number(document.querySelector('.highscore').textContent);
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showBtn = document.querySelectorAll('.show-modal');
 
-//document.querySelector('.number').textContent = secretNumber;
+for (let i = 0; i < showBtn.length; i++) {
+  showBtn[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
 
-checkBtn.addEventListener('click', function () {
-  let guess = Number(document.querySelector('.guess').value);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-  function newMessage(message) {
-    document.querySelector('.message').textContent = message;
-  }
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
 
-  function guessUpdate(guess) {
-    if (guess > secretNumber) {
-      newMessage('Too High!');
-      document.querySelector('.score').textContent = --score;
-    } else if (guess < secretNumber) {
-      newMessage('Too Low!');
-      document.querySelector('.score').textContent = --score;
-    } else if (guess === secretNumber) {
-      newMessage('Correct Answer');
-      highscore < score ? (highscore = score) : highscore;
-      document.querySelector('.highscore').textContent = highscore;
-      document.querySelector('.number').textContent = secretNumber;
-      document.querySelector('body').style.backgroundColor = 'green';
-      document.querySelector('.number').style.width = '30rem';
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
     }
   }
-
-  if (!guess) {
-    newMessage('You Must Guess...');
-  } else if (score > 1) {
-    guessUpdate(guess);
-  } else {
-    newMessage('You Loose');
-    document.querySelector('body').style.backgroundColor = 'red';
-    document.querySelector('.score').textContent = 0;
-  }
-});
-
-againBtn.addEventListener('click', function () {
-  document.querySelector('.message').textContent = 'Start Guessing..';
-  score = 20;
-  document.querySelector('.score').textContent = 20;
-  secretNumber = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
 });
