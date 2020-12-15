ce9cd8980fcec8067db93e0ffca3e6356df1324d
Vrushasen Dhere <vrushasen.dhere@lrn.com>
2020-12-15 08:19:03 +0530

    coding challenge 10 already done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,3 @@
 'use strict';
 
-// const arr = [17, 21, 23];
-const arr = [12, 5, -5, 0, 4];
-
-const printForecast = function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`${arr[i]}℃ in ${i + 1} days`);
-  }
-};
-
-printForecast(arr);
+console.log('already done in guess game');
